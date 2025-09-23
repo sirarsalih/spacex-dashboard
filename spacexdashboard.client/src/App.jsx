654d522cadd315e971cc4999b12fa4c2dbeb3198
Sirar Salih <sirar.salih@wintrytech.com>
2025-09-23 13:10:46 +0200

    Add live search input field that querys the data on client, work on styling.

--- spacexdashboard.client/src/App.jsx
+++ spacexdashboard.client/src/App.jsx
@@ -3,18 +3,33 @@ import './App.css';
 
 function App() {
     const [launches, setLaunches] = useState([]);
+    const [filteredLaunches, setFilteredLaunches] = useState([]);
     const [selectedLaunch, setSelectedLaunch] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchQuery, setSearchQuery] = useState("");
     const itemsPerPage = 20;
 
     useEffect(() => {
         populateRocketLaunchesData();
     }, []);
 
+    // Live search effect
+    useEffect(() => {
+        if (searchQuery.trim() === "") {
+            setFilteredLaunches(launches);
+        } else {
+            const filtered = launches.filter(l =>
+                l.name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setFilteredLaunches(filtered);
+            setCurrentPage(1);
+        }
+    }, [searchQuery, launches]);
+
     const indexOfLast = currentPage * itemsPerPage;
     const indexOfFirst = indexOfLast - itemsPerPage;
-    const currentLaunches = launches.slice(indexOfFirst, indexOfLast);
-    const totalPages = Math.ceil(launches.length / itemsPerPage);
+    const currentLaunches = filteredLaunches.slice(indexOfFirst, indexOfLast);
+    const totalPages = Math.ceil(filteredLaunches.length / itemsPerPage);
 
     const handleNext = () => {
         if (currentPage < totalPages) setCurrentPage(currentPage + 1);
@@ -46,39 +61,45 @@ function App() {
 
     return (
         <div>
-            {selectedLaunch ? (<h1 id="launchLabel" style={{ marginTop: '-10px', textAlign: 'center' }}>
-                <div>
-                    {selectedLaunch.links.patch.missionPatchSmall && (
-                        <img
-                            src={selectedLaunch.links.patch.missionPatchSmall}
-                            alt={`${selectedLaunch.name} patch`}                            
-                        />
-                    )}
-                </div>
-                {selectedLaunch.name}
-                {selectedLaunch.links?.webcast ? (() => {
-                    var videoUrl = "https://www.youtube.com/embed/" + selectedLaunch.links?.youTubeId + "?autoplay=1";
-                    return (
-                        <div style={{ marginTop: '20px', textAlign: 'center' }}>
-                            <iframe
-                                width="560"
-                                height="315"
-                                src={videoUrl}
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                            ></iframe>
-                        </div>
-                    );
-                })() : null}
-                </h1>)
-                : (<><img src="/spacex-logo-animation.gif" alt="App Logo" width="575" /><br /><div style={{ fontSize: '10px' }}>Design and animation by Dmitriy Ivanenko</div></>)}
+            {selectedLaunch ? (
+                <h1 id="launchLabel" style={{ marginTop: '-10px', textAlign: 'center' }}>
+                    <div>
+                        {selectedLaunch.links.patch.missionPatchSmall && (
+                            <img
+                                src={selectedLaunch.links.patch.missionPatchSmall}
+                                alt={`${selectedLaunch.name} patch`}
+                            />
+                        )}
+                    </div>
+                    {selectedLaunch.name}
+                    {selectedLaunch.links?.webcast ? (() => {
+                        var videoUrl = "https://www.youtube.com/embed/" + selectedLaunch.links?.youTubeId + "?autoplay=1";
+                        return (
+                            <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                                <iframe
+                                    width="560"
+                                    height="315"
+                                    src={videoUrl}
+                                    title="YouTube video player"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                ></iframe>
+                            </div>
+                        );
+                    })() : null}
+                </h1>
+            ) : (
+                <>
+                    <img src="/spacex-logo-animation.gif" alt="App Logo" width="400" style={{ marginLeft: '80px' }} />
+                    <div style={{ fontSize: '10px', marginLeft: '80px' }}>Logo design & animation by Dmitriy Ivanenko</div>
+                </>
+            )}
 
             {selectedLaunch ? (
                 <div style={{ marginTop: '20px', padding: '10px' }}>
                     <p><strong>Launch ID</strong>: {selectedLaunch.id}</p>
-                    <p><strong>Launch Time:</strong> {selectedLaunch.dateUtcRaw === "null" ? "TBD" : formatDate(selectedLaunch.dateUtcRaw)}</p>                   
+                    <p><strong>Launch Time:</strong> {selectedLaunch.dateUtcRaw === "null" ? "TBD" : formatDate(selectedLaunch.dateUtcRaw)}</p>
                     <p><strong>Outcome: </strong>
                         {selectedLaunch.success === null && selectedLaunch.dateUtc !== null
                             ? "Unknown"
@@ -93,7 +114,7 @@ function App() {
                         : "N/A"}</p>
                     <div style={{ textAlign: 'center' }}>
                         <button onClick={handleBackToList}>Back to List</button>
-                    </div>                    
+                    </div>
                 </div>
             ) : (
                 <>
@@ -101,9 +122,20 @@ function App() {
                         <p><em>Loading rocket launches...</em></p>
                     ) : (
                         <>
+                            {/* Live Search Input */}
+                            <div style={{ textAlign: 'center', marginBottom: '10px', marginTop: '15px' }}>
+                                <input
+                                    type="text"
+                                    value={searchQuery}
+                                    onChange={(e) => setSearchQuery(e.target.value)}
+                                    placeholder="Search by launch name..."
+                                    style={{ padding: '5px', width: '525px' }}
+                                />
+                            </div>
+
                             <table className="table table-striped" aria-labelledby="tableLabel">
                                 <thead>
-                                        <tr style={{ textAlign: 'left' }}>
+                                    <tr style={{ textAlign: 'left' }}>
                                         <th>Launch Name</th>
                                         <th>Launch Time</th>
                                         <th>Outcome</th>
@@ -115,8 +147,8 @@ function App() {
                                             key={launch.id}
                                             style={{ cursor: 'pointer' }}
                                             onClick={() => handleLaunchClick(launch.id)}
-                                        >                                       
-                                            <td>                                              
+                                        >
+                                            <td>
                                                 {launch.links.patch.missionPatchSmall && (
                                                     <img
                                                         src={launch.links.patch.missionPatchSmall}
@@ -145,7 +177,7 @@ function App() {
                                 </tbody>
                             </table>
 
-                                    <div style={{ marginTop: '10px', textAlign: 'center' }}>
+                            <div style={{ marginTop: '10px', textAlign: 'center' }}>
                                 <button onClick={handlePrev} disabled={currentPage === 1}>Previous</button>
                                 <span style={{ margin: '0 10px' }}>Page {currentPage} of {totalPages}</span>
                                 <button onClick={handleNext} disabled={currentPage === totalPages}>Next</button>
@@ -186,6 +218,7 @@ function App() {
                 if (response.ok) {
                     const data = await response.json();
                     setLaunches(data);
+                    setFilteredLaunches(data); // initialize filtered list
                     return; // Success, stop retrying
                 }
             } catch (err) {
